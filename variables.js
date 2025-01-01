94ddb1f78d176b8472cabff0396ef93071269cd7
Felipe coll <philipking3@philipking3.local>
2025-01-01 08:25:35 -0300

    Primeros pasos en javascript - 2

---
+++ variables.js
@@ -0,0 +1,18 @@
+// Declaracion de variables
+
+let cajaDeValores = 'Numeros'
+
+console.log(cajaDeValores)
+
+// No recomendado   
+
+let c = 'Valor'
+let cda = 'valor'
+let pcAndrea = 'valor'
+
+// Ideal
+
+let primerTesteoAndrea = 'valor'
+let urlDelUsuario = 'valor'
+let idUsuario = '12345'
+
