2171485b69d720634c3d44064315ca4940b4b1e6
Felipe Coll <devphilipcoll@gmail.com>
2025-01-01 15:57:00 -0300

    Primeros archivos de repaso en javascript

---
+++ tiposDeDatos.js
@@ -0,0 +1,48 @@
+// Datos primitivos
+
+// String 
+
+let nombre = 'Felipe'
+
+// Numericos
+
+let edad = 41
+
+// Booleanos 
+
+let esMayorDeEdad = true // false 
+
+// Null
+
+let noHayValor = null
+
+// Undefined 
+
+let noDefinido = undefined
+
+// Symbol
+
+let simboloUnico = Symbol('unico')
+
+// BigInt 
+
+let numeroGrande = 2311460984691640141
+
+// Complejos
+
+// Object 
+
+let auto = {
+    marca: 'Tesla', 
+    modelo: 'Model S'
+}
+
+// Arrays
+
+let frutas = ['manzana', 'pera', 'uva'] 
+
+// Function - Funciones 
+
+function saludar () {
+    console.log('Hola perri')
+}
--- variables.js
+++ variables.js
@@ -16,3 +16,19 @@ let primerTesteoAndrea = 'valor'
 let urlDelUsuario = 'valor'
 let idUsuario = '12345'
 
+
+// Ejemplo practico general de la clase
+
+let puertas = 5 
+let ruedas = 4 
+let espejosRetrovisores = 2 
+let espejoCocheInterior = 1 
+let asientosSimples = 2 
+let asientosPasajeros = 3
+let volante = 1 
+let cajaDeCambios = 1 
+let motor = 1 
+let escape = 1 
+let carroceria = 1
+
+const automovil = 'VW Polo'
