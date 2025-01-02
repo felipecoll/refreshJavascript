64c9353f94b37df55c23a63a556c872e85182a24
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-02 18:39:59 -0300

    Terminada clase Strings, se procede a numbers

--- strings.js
+++ strings.js
@@ -1,3 +1,17 @@
 // Tipo de dato: string 
 
-console.log('en espera de la clase')
+// let string1 = 'Hola mundo!'
+// let string2 = 'Javascript domina!!'
+// let string3 = `${string1} feliz:`
+// let string4 = string1 + ' ' + string2
+
+let frase = 'Javascript es extremadamente genial'
+
+console.log(frase.length)
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())
+console.log(frase.substring(0,10))
+// console.log(string1)
+// console.log(string2)
+// console.log(string3)
+// console.log(string4)
