8f77587eb6f0e5cdac11633f574f5bc7fbc747df
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-03 16:20:58 -0300

    Actualizacion en refresh Javascript

---
+++ Operadores.js
@@ -0,0 +1,20 @@
+// Operadores de comparacion
+
+// ==  es igual a 
+// === es igual a y del mismo tipo
+// != es diferente de 
+// !== es diferente de y del mismo tipo
+// > es mayor que
+// < es menor que 
+// >= es mayor o igual que 
+// <= es menor o igual que 
+
+const a = 10
+const b = 20
+const c = '10'
+
+a == b
+
+
+
+
---
+++ operadoresLogicos.js
@@ -0,0 +1,14 @@
+// Operadores logicos 
+
+// && and o mas conocido como y 
+// || or o mas conocido como o
+// ! not o mas conocido como valor de negacion 
+
+// Ejercitacion 
+
+const a = 10
+const b = 20
+const c = '10'
+
+
+
