189a58ad27ccbe8ec7b6e9f0d6e6eb2f7ad49914
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-09 18:43:45 -0300

    Uso de slice

---
+++ slice.js
@@ -0,0 +1,10 @@
+// Uso de slice ==> .slice(start, end)
+
+const animals = ['Hormiga', 'Pato', 'Elefante', 'Perro']
+
+console.log(animals.slice(2))
+console.log(animals.slice(2,3))
+
+// Cuenta o uso slice desde el final del array
+console.log(animals.slice(-2))
+console.log(animals.slice(2, -1))
