036c265cc3710684dc08bdaea41b851c765ae29c
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-12 07:40:50 -0300

    Clases - usos

---
+++ clases.js
@@ -0,0 +1,15 @@
+// Clases
+
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre,
+        this.edad = edad
+    }
+    saludar() {
+        console.log(`Hola, mi nombre es: ${this.nombre} y tengo ${this.edad} años.`)
+    }
+}
+
+const persona1 = new Persona('Angelica', 40)
+
+persona1.saludar()
