0c6c1284dc0f1035b4e797ca2558e43c2bef31ba
Felipe Coll <leonfelipecoll@gmail.com>
2025-01-15 17:24:02 -0300

    Async y Await - Uso de for Await

--- asyncAwait.js
+++ asyncAwait.js
@@ -46,3 +46,32 @@ Async/await es una sintaxis que hace que el código asíncrono sea más fácil d
 
 
  */
+
+const urls = ["https://rickandmortyapi.com/api/character",
+              "https://rickandmortyapi.com/api/location",
+              "https://rickandmortyapi.com/api/episode"
+]
+
+async function fetchNewData() {
+    try {
+        for await (url of urls) {
+            let response = await fetch(url)
+            let data = await response.json()
+            console.log(data)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+/*
+
+for await...of es una estructura de control en JavaScript utilizada para iterar sobre objetos iterables asíncronos. Permite procesar elementos de forma secuencial, esperando la resolución de cada elemento antes de pasar al siguiente.
+
+for await...of es una herramienta poderosa para trabajar con datos asíncronos en JavaScript. Permite iterar sobre elementos de forma secuencial, esperando la resolución de cada elemento antes de continuar, lo que simplifica el manejo de operaciones asíncronas en comparación con el uso de promesas y bucles tradicionales.
+
+Diferencia con for...of:
+
+Mientras que for...of se utiliza para iterar sobre objetos iterables síncronos, for await...of está diseñado específicamente para manejar iterables asíncronos.
+
+*/
